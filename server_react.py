b1e101adf00318042b0a9336df80dd3818953e52
li-lauren <laurenlidev@gmail.com>
2020-11-02 04:17:35 +0000

    Incorporate connect, disconnect, and join room

--- server_react.py
+++ server_react.py
@@ -1,10 +1,15 @@
-from flask import Flask, jsonify, render_template;
-from flask_socketio import SocketIO, send
+from flask import Flask, jsonify, render_template, request
+from flask_socketio import SocketIO, send, join_room
+
+import os
+
+#SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = 'blahblah'
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'mysecret'
+app.secret_key = SECRET_KEY
 
-socketIo = SocketIO(app, cors_allowed_origins="*")
+io = SocketIO(app, cors_allowed_origins="*")
 
 # app.debug = True
 # app.host = 'localhost'
@@ -12,11 +17,25 @@ socketIo = SocketIO(app, cors_allowed_origins="*")
 def index():
     return render_template('index_react.html')
 
-@socketIo.on("message")
+@io.on("connect")
+def test_connect():
+    print(f"Client connected")
+
+@io.on("disconnect")
+def test_disconnect():
+    print("Client disconnected")
+
+@io.on("join")
+def handle_join_room(room):
+    print(f"User is now in Room {room}, SID: {request.sid}")
+    join_room(room)
+    io.emit('join_msg', room=room)
+
+@io.on("message")
 def handleMessage(msg):
     print(msg)
     send(msg, broadcast=True)
-    return None
+    return None   
 
 if __name__ == '__main__':
-    socketIo.run(app, debug=True, host='0.0.0.0')
+    io.run(app, debug=True, host='0.0.0.0')
--- static/js/App.jsx
+++ static/js/App.jsx
@@ -1,7 +1,54 @@
 // Connect with server via socket io
-let socket = io.connect('http://0.0.0.0:5000/')
+//let socket = io.connect('http://0.0.0.0:5000/')
 
+let socket;
 
+const connectSocket = (room) => {
+    socket = io.connect('http://0.0.0.0:5000/');
+    console.log('Connecting...');
+    if (socket && room) {
+        socket.emit('join', room)
+    }
+}
+
+const disconnectSocket = () => {
+    console.log('Disconnecting...')
+    if (socket) {
+        socket.disconnect();
+    }
+}
+
+const JoinRoom = () => {
+    const rooms = [1, 2, 3, 4, 5]
+    const [user, setUser] = React.useState('');
+    const [room, setRoom] = React.useState(rooms[0]);
+
+    React.useEffect(() => {
+        if (room) {
+            connectSocket(room)
+            console.log(`Joined Room ${room}`)
+        };
+
+        return () => {
+            disconnectSocket()
+        }
+    }, [room]);
+
+    const getUser = e => {
+        setUser(e.target.value);
+    };
+
+    return (
+        <React.Fragment>
+            User
+            <input value={user} name="user" onChange={e => getUser(e)} />
+            <p>Room: {room} </p>
+            {rooms.map((room, i) =>
+                <button onClick={()=> setRoom(room)} key={i}>{room}</button>
+            )}
+        </React.Fragment>  
+    )
+}
 const App = () => {
     const [messages, setMessages] = React.useState(["Hello And Welcome"]);
     const [message, setMessage] = React.useState("");
@@ -36,6 +83,7 @@ const App = () => {
   
     return (
       <div>
+        <JoinRoom />
         {messages.length > 0 &&
           messages.map(msg => (
             <div>
